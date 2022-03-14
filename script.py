bbc8e98dff6f00c4d8c2fe3741cac13bc2b04f27
Polina Pumova <74357600+Pumovka@users.noreply.github.com>
2022-03-14 17:15:31 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
+from array import array
+import string
+
+
 class Student:
     def SetStudent(self, name, last_name, year, number_course, number_group, marks ):
         self.name  = name
@@ -6,9 +10,12 @@ class Student:
         self.number_course = number_course 
         self.number_group = number_group 
         self.marks = marks
+        print(name, last_name, year, number_course, number_group, marks)
+    pass
+    def printStudent(self):
         print(self.name, self.last_name, self.year, self.number_course, self.number_group, self.marks)
-        pass
-    
+    pass
+  
 student1 = Student()
 student2 = Student()
 student3 = Student()
@@ -21,14 +28,37 @@ student3.SetStudent("Петр", "Петров", 1994, 3, "ВПИ-20", [4,3,5,4,4
 student4.SetStudent("Егоров", "Егор", 1999, 1, "ВПИ-21", [3,3,3,5,3])
 student5.SetStudent("Максимов", "Максим", 1997, 1, "ВПИ-21", [4,4,5,5,3])
 
-list = [student1,student2, student3, student4, student5]
+list = [student1,student2,student3,student4,student5]
 
-def SortCourse():
-    for i in range(0, len(list)- 1):
-        for j in range(0, len(list)-1-i):
-            if(list[j].number_course > list[j+1].number_course ):
+
+def SortCourse(list):#Сортировка по году рождения
+    i = 0
+    for i in range(len(list)):
+        for j in range(0, len(list)-i-1):
+            if list[j].number_course > list[j+1].number_course:
                 list[j], list[j+1] = list[j+1], list[j]
-    print(list)
- 
+pass
 
-SortCourse()
+SortCourse(list)
+
+print("1. Упорядочить студентов по курсу (без учета алфавитного порядка)")
+
+def printList(list):
+    i=0
+    for i in range(len(list)):
+        list[i].printStudent()
+pass
+
+printList(list)
+
+def SortAge(list): #Сортировка по году рождения
+        for i in range(len(list)):
+            for j in range(0, len(list)-i-1):
+                if list[j].year > list[j+1].year:
+                    list[j], list[j+1] = list[j+1], list[j]
+pass
+
+SortAge(list)
+print("3. Найти самого старшего и самого младшего")
+print("Самый старший:", list[0].name , list[0].last_name , list[0].year)
+print("Самый младший:", list[len(list)-1].name , list[len(list)-1].last_name , list[len(list)-1].year)
